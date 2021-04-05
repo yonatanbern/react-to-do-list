11445a745f95b1e3d14a4a16085cc696f19bdc6a
yonatanberns <yonatan.bernstein5@gmail.com>
2021-04-05 14:53:31 +0300

    form component 1.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/form'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> to do list.....!!! 
-          another change
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      new form
+      <Form/>
     </div>
   );
 }
---
+++ src/components/form.jsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+const Form = ({addTodo})=> {
+
+    const [user_input, set_user_input] = useState("");
+
+    const handleChange = (e)=> {
+        set_user_input(e.target.value);
+    }
+
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        console.log('submit', user_input);
+        user_input.length > 0 ? console.log('add to list->', addTodo(user_input)) : console.log('nothing');
+        set_user_input("");
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={user_input} placeholder="enter new task.." onChange={handleChange}/>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
