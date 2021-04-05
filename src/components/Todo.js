3562781f31b695a8cccb5bcdf556b2f8211c4f30
tomerkeizler <tomerk@rapyd.ac>
2021-04-05 17:24:40 +0300

    deleted unnecessary styling, and corrected onChanhe event in checkbox

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Todo = ({ index, todo, removeTodo, toggleTodo }) => {
     return (
     <div style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
-        <input type="checkbox" name="isCompleted" value={todo.isCompleted} checked={todo.isCompleted} onClick={() => toggleTodo(index)}/>
+        <input type="checkbox" name="isCompleted" value={todo.isCompleted} checked={todo.isCompleted} onChange={() => toggleTodo(index)}/>
         {todo.text}
         <button onClick={() => removeTodo(index)}>X</button>
     </div>
